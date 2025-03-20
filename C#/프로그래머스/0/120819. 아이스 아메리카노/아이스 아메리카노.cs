99cddbe6ef3be71a9653a084139ef067ccb8d611
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-03-20 17:50:13 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

--- C#/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cs
+++ C#/프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cs
@@ -2,9 +2,8 @@ using System;
 
 public class Solution {
     public int[] solution(int money) {
-        int[] answer = new int[2];
-        answer[0] += money/5500;
-        answer[1] += money%5500;
+        int[] answer = new int[2] {money/5500, money%5500};
+        
         return answer;
     }
 }
