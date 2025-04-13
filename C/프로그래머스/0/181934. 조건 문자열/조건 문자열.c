86ead4c319384aea266ca0d23035086da286a125
sejun-2 <166081523+sejun-2@users.noreply.github.com>
2025-04-13 20:31:11 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- C/프로그래머스/0/181934. 조건 문자열/조건 문자열.c
+++ C/프로그래머스/0/181934. 조건 문자열/조건 문자열.c
@@ -5,26 +5,17 @@
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* ineq, const char* eq, int n, int m) {
     int answer = 0;
-    
-    if(*ineq == '>'){
-        if(*eq == '='){
-            if(n>=m){
-                answer = 1;
-            }
-        } else if(*eq == '!'){
-            if (n > m) {
-                answer = 1;
-            }
+    if (*ineq == '>') {
+        if (*eq == '=') {
+            if (n >= m) answer = 1;
+        } else if (*eq == '!') {
+            if (n > m) answer = 1;
         }
-    } else if(*ineq == '<'){
-        if(*eq == '='){
-            if(n<=m){
-                answer = 1;
-            }
-        } else if(*eq == '!'){
-            if (n < m) {
-                answer = 1;
-            }
+    } else if (*ineq == '<') {
+        if (*eq == '=') {
+            if (n <= m) answer = 1;
+        } else if (*eq == '!') {
+            if (n < m) answer = 1;
         }
     }
          return answer; 
